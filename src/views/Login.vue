7a9ec00e44fc1fd51aa90317fdbf5fa65e957914
muti <mustafakendiguzel@gmail.com>
2024-01-18 01:59:02 +0300

    add dont have an account container

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="container">
-    <input type="text" class="centered-input" placeholder="Enter your text" />
-    <input type="text" class="centered-input" placeholder="Enter your text" />
+  <div class="logo-with-text-inputs">
+    <div class="auth-logo">
+      <img alt="Vue logo" src="@/assets/working-with-laptop.svg" width="500" height="500" />
+    </div>
+    <div>
+      <div class="input-boxes">
+        <div class="input-container">
+          <div class="container">
+            <div class="input-container-text">
+              <input type="text" class="centered-input" placeholder="Email" />
+            </div>
+            <div class="input-container-text">
+              <input type="text" class="centered-input" placeholder="Şifre" />
+            </div>
+          </div>
+        </div>
+        <div class="register-container">
+          <div class="centered-text">
+            Hesabın Yok mu? <router-link to="/register" class="register-link">Kaydol</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,9 +31,74 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  height: 100%;
+  gap: 2em;
+}
+
+.input-container {
+  width: 400px;
+  height: 400px;
+  border: 2px solid white;
+  max-width: 400px; /* Maksimum genişlik belirleme */
+}
+
+.input-boxes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2em;
+}
+
+.register-container {
+  width: 400px;
+  height: 100px;
+  border: 2px solid white;
+  max-width: 400px; /* Maksimum genişlik belirleme */
+}
+
+.logo-with-text-inputs {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8em;
+  padding-top: 300px;
+}
+.text-with-box {
+  display: flex;
+  gap: 4em;
+  align-items: center;
 }
 
 .centered-input {
+  flex: 1;
   text-align: center;
+  box-sizing: border-box;
+  width: 70%;
+}
+
+.input-container-text {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.centered-text {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+@media only screen and (max-width: 768px) {
+  .auth-logo {
+    display: none;
+  }
+
+  .input-container,
+  .register-container {
+    max-width: 100%;
+  }
 }
 </style>
